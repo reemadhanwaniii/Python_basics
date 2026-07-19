8680f0cd05f1f80ff527c4f53324cc20f6f1131d
Reema Dhanwani <reemadhanwani55@gmail.com>
2026-07-18 22:04:18 -0700

    added list implementation

--- functions.py
+++ functions.py
@@ -32,3 +32,42 @@ print(result)
 
 
 print(pow(2,3)) # predefined function
+
+
+
+# python list
+# used to store multiple data at once
+
+number = [1,2,4,6]
+print(number)
+
+my_list = [1,"hello",3.15]
+print(my_list)
+
+
+my_list.append("78")
+print(my_list)
+
+
+prime_number = [2,3,5]
+even_number =[4,6,8]
+odd_number = [1,3,5]
+
+prime_number.extend(even_number)
+print(prime_number)
+
+my_list[1] = 54
+print(my_list)
+
+# delete something from list
+del my_list[1]
+print(my_list)
+
+my_list.remove("78")
+print(my_list)
+
+print(3.14 in my_list)
+print(3.15 in my_list)
+
+
+print(len(my_list))
