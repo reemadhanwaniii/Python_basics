42342dc732766bbe6bf3f73a10881d2338f0fcb6
Reema Dhanwani <reemadhanwani55@gmail.com>
2026-07-19 00:48:39 -0700

    added modules and packages

---
+++ project/utils/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py tells python that the folder should be treated as  package
+# in modern python, it can be sometimes be optimal, but it is still commonly used
+
+# absolute imports : uses full path to package, project , root
